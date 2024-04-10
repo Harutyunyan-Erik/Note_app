dc9d5f6549846ce442c9f253dbff35aab3d397e8
Harutyunyan-Erik <erik2698@gmail.com>
2024-04-11 02:58:14 +0400

    2

--- script/constants.js
+++ script/constants.js
@@ -1,3 +1,5 @@
 export const workspace = document.getElementById("workspace");
 export const btn = document.getElementById("btn");
 export const notes = document.getElementById("notes");
+export const textArea = document.createElement("textarea");
+
--- script/script.js
+++ script/script.js
@@ -1,16 +1,31 @@
-import { btn, workspace, notes } from "./constants.js";
+import { btn, workspace, notes, textArea } from "./constants.js";
+const taskList = JSON.parse(localStorage.getItem('tasklist')) || [] ;
 
 
 function noteCreator() {
-    const textArea = document.createElement("textarea");
     textArea.className = "notes";
     textArea.placeholder = "Empty Note";
     workspace.appendChild(textArea);
     workspace.appendChild(btn);
+    taskList.push(textArea.value);
+    changeLocaleStorage();
+    console.log(taskList);
 }
 
 btn.addEventListener("click", noteCreator);
 
-notes.addEventListener("dblclick", ()=> {
-    console.log("piti jnjvi");
+workspace.addEventListener("dblclick", (e)=> {
+    if (e.target.classList.contains('notes')) {
+        const index = taskList.indexOf(e.target.value); // Find the index of the note
+        taskList.splice(index, 1); // Remove the note from the taskList array
+        e.target.remove();
+        changeLocaleStorage();
+    }
 })
+
+
+function changeLocaleStorage(){
+    localStorage.setItem('taskList', JSON.stringify(taskList));
+}
+
+//-------------------------------------------------
